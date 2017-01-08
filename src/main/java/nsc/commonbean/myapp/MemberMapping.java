4335f50ae4ed4f2ab782d45138fd9a722c75d60a
chaluemwut <chaluemwut@hotmail.com>
2017-01-08 08:00:21 +0700

    add member mapping

---
+++ src/main/java/nsc/commonbean/myapp/MemberMapping.java
@@ -0,0 +1,14 @@
+package nsc.commonbean.myapp;
+
+public class MemberMapping {
+	private int role;
+
+	public int getRole() {
+		return role;
+	}
+
+	public void setRole(int role) {
+		this.role = role;
+	}
+
+}
