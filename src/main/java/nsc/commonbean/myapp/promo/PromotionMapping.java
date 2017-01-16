0a0aa5ed4be2ddb347d121a9d62508596d3ed40d
chaluemwut <chaluemwut@hotmail.com>
2017-01-17 00:39:11 +0700

    add promotion

---
+++ src/main/java/nsc/commonbean/myapp/promo/PromotionMapping.java
@@ -0,0 +1,36 @@
+package nsc.commonbean.myapp.promo;
+
+import java.util.Date;
+
+public class PromotionMapping {
+	private int id;
+	private String title;
+	private String message;
+	private Date createDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+}
