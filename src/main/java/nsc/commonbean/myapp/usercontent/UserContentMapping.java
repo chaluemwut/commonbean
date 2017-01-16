2dfdaa448fddd79e84ce4038cd176a6b8b92c4a8
chaluemwut <chaluemwut@hotmail.com>
2017-01-16 20:55:55 +0700

    add user content

---
+++ src/main/java/nsc/commonbean/myapp/usercontent/UserContentMapping.java
@@ -0,0 +1,50 @@
+package nsc.commonbean.myapp.usercontent;
+
+import java.util.Date;
+
+public class UserContentMapping {	
+	private int id;	
+	private String message;
+	private Date createDate;
+	private int likeCount;
+	private int commentCount;
+	private int shareCount;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public int getLikeCount() {
+		return likeCount;
+	}
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+	public int getCommentCount() {
+		return commentCount;
+	}
+	public void setCommentCount(int commentCount) {
+		this.commentCount = commentCount;
+	}
+	public int getShareCount() {
+		return shareCount;
+	}
+	public void setShareCount(int shareCount) {
+		this.shareCount = shareCount;
+	}
+	
+}
