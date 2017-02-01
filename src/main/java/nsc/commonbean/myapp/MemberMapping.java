03ec32edcece19c2bd5b124148269f572faed170
chaluemwut <chaluemwut@hotmail.com>
2017-02-01 22:26:27 +0700

    #

--- src/main/java/nsc/commonbean/myapp/MemberMapping.java
+++ src/main/java/nsc/commonbean/myapp/MemberMapping.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class MemberMapping implements Serializable{
 	private static final long serialVersionUID = -4416251441320316997L;
-	
+	private int userId;
 	private int role;
 
 	public int getRole() {
@@ -15,4 +15,12 @@ public class MemberMapping implements Serializable{
 		this.role = role;
 	}
 
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
 }
