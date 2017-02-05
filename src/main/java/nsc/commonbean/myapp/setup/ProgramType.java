53f4baa46a3c484dc3ca2b1ff84e9d4a7b91fefa
chaluemwut <chaluemwut@hotmail.com>
2017-02-05 10:38:30 +0700

    add setup

---
+++ src/main/java/nsc/commonbean/myapp/setup/ProgramType.java
@@ -0,0 +1,50 @@
+package nsc.commonbean.myapp.setup;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ProgramType implements Serializable{
+	private static final long serialVersionUID = -6704479712865622849L;
+
+	private int id;
+
+	private String name;
+
+	private String detail;
+
+	private Date createDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+}
