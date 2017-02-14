160bbe4b7168ba48cf772b978930f7b6c58b7c35
chaluemwut <chaluemwut@hotmail.com>
2017-02-14 20:12:25 +0700

    add program mapping

---
+++ src/main/java/nsc/commonbean/myapp/setup/ProgramTypeMapping.java
@@ -0,0 +1,46 @@
+package nsc.commonbean.myapp.setup;
+
+import java.io.Serializable;
+
+
+public class ProgramTypeMapping implements Serializable {
+	private static final long serialVersionUID = -8494573407370122720L;
+	
+	private int id;
+	private String name;	
+	private int displayOrder;
+	private int activityType;	
+	private String detail;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDisplayOrder() {
+		return displayOrder;
+	}
+	public void setDisplayOrder(int displayOrder) {
+		this.displayOrder = displayOrder;
+	}
+	public int getActivityType() {
+		return activityType;
+	}
+	public void setActivityType(int activityType) {
+		this.activityType = activityType;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+}
