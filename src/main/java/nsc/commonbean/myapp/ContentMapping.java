a6db3c26b46e0c527756d518ecc9255611782c20
chaluemwut <chaluemwut@hotmail.com>
2017-03-05 14:12:51 +0700

    content new feature > dashboard

--- src/main/java/nsc/commonbean/myapp/ContentMapping.java
+++ src/main/java/nsc/commonbean/myapp/ContentMapping.java
@@ -10,6 +10,12 @@ public class ContentMapping implements Serializable {
 	private String detail;
 	private int numberOfImage;
 	private BigDecimal price;
+	
+	private int suggest;
+	private int discount;
+	private int newProduct;
+	private int showToDashboard;	
+	
 
 	public int getProductId() {
 		return productId;
@@ -49,6 +55,38 @@ public class ContentMapping implements Serializable {
 
 	public void setNumberOfImage(int numberOfImage) {
 		this.numberOfImage = numberOfImage;
+	}
+
+	public int getSuggest() {
+		return suggest;
+	}
+
+	public void setSuggest(int suggest) {
+		this.suggest = suggest;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public int getNewProduct() {
+		return newProduct;
+	}
+
+	public void setNewProduct(int newProduct) {
+		this.newProduct = newProduct;
+	}
+
+	public int getShowToDashboard() {
+		return showToDashboard;
+	}
+
+	public void setShowToDashboard(int showToDashboard) {
+		this.showToDashboard = showToDashboard;
 	}
 
 
