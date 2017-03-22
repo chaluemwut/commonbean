27d7f14b57473da4f4efc2aaf4585b22548e93a2
chaluemwut <chaluemwut@hotmail.com>
2017-03-22 22:54:46 +0700

    content comment mapping

---
+++ src/main/java/nsc/commonbean/myapp/usercontent/UserContentCommentMapping.java
@@ -0,0 +1,46 @@
+package nsc.commonbean.myapp.usercontent;
+
+import java.io.Serializable;
+
+/**
+ * Created by chaluemwutnoyunsan on 3/22/17.
+ */
+public class UserContentCommentMapping implements Serializable{
+    private int id;
+    private String message;
+
+    private int userId;
+    private String userName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
