cf1bd5623dff5ba9b6bae66a7cbae63c3a3a850c
chaluemwut <chaluemwut@hotmail.com>
2017-05-16 19:56:16 +0700

    add common config

---
+++ src/main/java/nsc/commonbean/myapp/CommonConfig.java
@@ -0,0 +1,15 @@
+package nsc.commonbean.myapp;
+
+/**
+ * Created by chaluemwutnoyunsan on 5/16/17.
+ */
+public class CommonConfig {
+
+    public static final int NESTSTARTUP_USER_TYPE = 1;
+    public static final int FACEBOOK_USER_TYPE = 2;
+
+    public static final int GENERAL_USER_ROLE = 3;
+    public static final int ADMIN_USER_ROLE = 2;
+    public static final int SUPER_USER_ROLE = 1;
+
+}
