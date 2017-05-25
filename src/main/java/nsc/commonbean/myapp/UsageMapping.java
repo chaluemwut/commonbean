b81383552bb99a90db5d78191c3b0955d888ddb3
chaluemwut <chaluemwut@hotmail.com>
2017-05-25 19:22:54 +0700

    usage mapping

---
+++ src/main/java/nsc/commonbean/myapp/UsageMapping.java
@@ -0,0 +1,33 @@
+package nsc.commonbean.myapp;
+
+import java.io.Serializable;
+
+public class UsageMapping implements Serializable {
+    private String dateExpire;
+    private int usage;
+    private int limit;
+    
+    public String getDateExpire() {
+        return dateExpire;
+    }
+
+    public void setDateExpire(String dateExpire) {
+        this.dateExpire = dateExpire;
+    }
+
+    public int getUsage() {
+        return usage;
+    }
+
+    public void setUsage(int usage) {
+        this.usage = usage;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+}
