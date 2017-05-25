072be1ba78e3ebb48274569b918439de526cf311
chaluemwut <chaluemwut@hotmail.com>
2017-05-25 21:30:42 +0700

    #

--- src/main/java/nsc/commonbean/myapp/BuyListMapping.java
+++ src/main/java/nsc/commonbean/myapp/BuyListMapping.java
@@ -4,43 +4,50 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 public class BuyListMapping implements Serializable {
-	private static final long serialVersionUID = -1210984722949450140L;
-	
-	private int id;
-	private String name;
-	private int buyListType;
-	private int itemCount;
-	private BigDecimal price;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getItemCount() {
-		return itemCount;
-	}
-	public void setItemCount(int itemCount) {
-		this.itemCount = itemCount;
-	}
-	public BigDecimal getPrice() {
-		return price;
-	}
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-	public int getBuyListType() {
-		return buyListType;
-	}
-	public void setBuyListType(int buyListType) {
-		this.buyListType = buyListType;
-	}
+    private int id;
+    private String name;
+    private int buyListType;
+    private int itemCount;
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getBuyListType() {
+        return buyListType;
+    }
+
+    public void setBuyListType(int buyListType) {
+        this.buyListType = buyListType;
+    }
 
 }
