cd11f536893e729102ef343f84d0186f8de85eda
chaluemwut <chaluemwut@hotmail.com>
2017-07-26 15:00:21 +0700

    Add company update

---
+++ src/main/java/nsc/commonbean/myapp/CompanyUpdateMapping.java
@@ -0,0 +1,23 @@
+package nsc.commonbean.myapp;
+
+import java.io.Serializable;
+
+public class CompanyUpdateMapping implements Serializable {
+	public boolean isUpdate;
+	public String updateId;
+	
+	public boolean isUpdate() {
+		return isUpdate;
+	}
+	public void setUpdate(boolean isUpdate) {
+		this.isUpdate = isUpdate;
+	}
+	public String getUpdateId() {
+		return updateId;
+	}
+	public void setUpdateId(String updateId) {
+		this.updateId = updateId;
+	}
+	
+	
+}
