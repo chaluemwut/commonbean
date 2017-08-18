c7f4ad523cfd95207561e6f58a83fe0cf013de70
chaluemwut <chaluemwut@hotmail.com>
2017-08-18 12:11:03 +0700

    Add Tiny constructor

--- src/main/java/nsc/commonbean/myapp/TinyMemberMapping.java
+++ src/main/java/nsc/commonbean/myapp/TinyMemberMapping.java
@@ -4,6 +4,10 @@ public class TinyMemberMapping {
 	private int userId;
 	private String userName;
 	
+	public TinyMemberMapping(int userId, String userName) {
+		this.userId = userId;
+		this.userName = userName;
+	}
 	public int getUserId() {
 		return userId;
 	}
