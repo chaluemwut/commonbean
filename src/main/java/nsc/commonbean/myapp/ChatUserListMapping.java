ae33e6e4462b3c349c2ecc3f89f3ace201f9e692
chaluemwut <chaluemwut@hotmail.com>
2017-08-26 17:26:14 +0700

    Add chat admin mapping

---
+++ src/main/java/nsc/commonbean/myapp/ChatUserListMapping.java
@@ -0,0 +1,44 @@
+package nsc.commonbean.myapp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ChatUserListMapping implements Serializable {
+	private int userId;
+	private String userName;
+	private String resourceId;
+	private Date createDate;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getResourceId() {
+		return resourceId;
+	}
+
+	public void setResourceId(String resourceId) {
+		this.resourceId = resourceId;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+}
