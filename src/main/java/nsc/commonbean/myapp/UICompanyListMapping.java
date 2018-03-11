105787926f432283466be9d73288b989bc6ffbbd
chaluemwuut <chaluemwuut@iMac-khxng-chaluemwuut.local>
2018-03-11 13:06:40 +0700

    add ui admin list

---
+++ src/main/java/nsc/commonbean/myapp/UICompanyListMapping.java
@@ -0,0 +1,60 @@
+package nsc.commonbean.myapp;
+
+import java.io.Serializable;
+
+public class UICompanyListMapping implements Serializable {
+    private int id;
+    private String name;
+    private String packageName;
+    private String buyStatus;
+    private int diskUsage;
+    private int remanderDay;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public String getBuyStatus() {
+        return buyStatus;
+    }
+
+    public void setBuyStatus(String buyStatus) {
+        this.buyStatus = buyStatus;
+    }
+
+    public int getDiskUsage() {
+        return diskUsage;
+    }
+
+    public void setDiskUsage(int diskUsage) {
+        this.diskUsage = diskUsage;
+    }
+
+    public int getRemanderDay() {
+        return remanderDay;
+    }
+
+    public void setRemanderDay(int remanderDay) {
+        this.remanderDay = remanderDay;
+    }
+}
