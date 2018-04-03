6dee9489b44bd6212a3951ac77c0af64c6f5eeb6
chaluemwuut <chaluemwuut@iMac-khxng-chaluemwuut.local>
2018-04-03 17:27:41 +0700

    Add chat 2 mapping

---
+++ src/main/java/nsc/commonbean/myapp/Chat2Mapping.java
@@ -0,0 +1,51 @@
+package nsc.commonbean.myapp;
+
+import java.io.Serializable;
+
+public class Chat2Mapping implements Serializable {
+    private int productId;
+    private String productName;
+    private int price;
+    private int itemCount;
+    private int chatType;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public int getChatType() {
+        return chatType;
+    }
+
+    public void setChatType(int chatType) {
+        this.chatType = chatType;
+    }
+}
