aa27195a70114767cc7ea43b960f2a7c72a65b25
chaluemwuut <chaluemwut@hotmail.com>
2018-06-02 02:00:01 +0700

    fix offline data

--- src/main/java/nsc/commonbean/myapp/OfflineMapping.java
+++ src/main/java/nsc/commonbean/myapp/OfflineMapping.java
@@ -3,40 +3,59 @@ package nsc.commonbean.myapp;
 import java.io.Serializable;
 
 public class OfflineMapping implements Serializable {
-    private Integer themeId;
-    private String splashResourceId;
+	private Integer themeId;
+	private String splashResourceId;
 
-    public Integer getThemeId() {
-        return themeId;
-    }
+	public Integer getThemeId() {
+		return themeId;
+	}
 
-    public void setThemeId(Integer themeId) {
-        this.themeId = themeId;
-    }
+	public void setThemeId(Integer themeId) {
+		this.themeId = themeId;
+	}
 
-    public String getSplashResourceId() {
-        return splashResourceId;
-    }
+	public String getSplashResourceId() {
+		return splashResourceId;
+	}
 
-    public void setSplashResourceId(String splashResourceId) {
-        this.splashResourceId = splashResourceId;
-    }
+	public void setSplashResourceId(String splashResourceId) {
+		this.splashResourceId = splashResourceId;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((splashResourceId == null) ? 0 : splashResourceId.hashCode());
+		result = prime * result + ((themeId == null) ? 0 : themeId.hashCode());
+		return result;
+	}
 
-        OfflineMapping that = (OfflineMapping) o;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OfflineMapping other = (OfflineMapping) obj;
+		if (splashResourceId == null) {
+			if (other.splashResourceId != null) {
+				return false;
+			}
+		} else if (!splashResourceId.equals(other.splashResourceId)) {
+			return false;
+		}
 
-        if (!themeId.equals(that.themeId)) return false;
-        return splashResourceId.equals(that.splashResourceId);
-    }
+		if (themeId == null) {
+			if (other.themeId != null) {
+				return false;
+			}
+		} else if (!themeId.equals(other.themeId)) {
+			return false;
+		}
+		return true;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = themeId.hashCode();
-        result = 31 * result + splashResourceId.hashCode();
-        return result;
-    }
 }
